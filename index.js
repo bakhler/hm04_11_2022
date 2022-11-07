a3c91645d7121ffcaa4d9c907845c5df58b822a2
bakhler <thehenridit@gmail.com>
2022-11-07 08:36:55 +0200

    Create let veriables

---
+++ index.js
@@ -0,0 +1,9 @@
+let personsAge = '23';
+console.log(personsAge);
+
+let isPersonAnAdult = true;
+
+let progrmaingLanguageName = 'C++';
+console.log(progrmaingLanguageName);
+
+let doPersonHaveAnAnimal = null;
